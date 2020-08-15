1dc35156ab41ec317da06617e6320307df31928c
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-08-15 17:08:28 +0200

    Adding MVVM structure

---
+++ rzeczuchyTasks/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rzeczuchyTasks.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private bool IsInDesignMode()
+        {
+            return DesignerProperties.GetIsInDesignMode(new DependencyObject());
+        }
+    }
+}
