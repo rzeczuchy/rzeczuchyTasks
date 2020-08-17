3b832b60e3a50ede68130819603d70be102e4282
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-08-17 07:58:05 +0200

    Moving AddToDoCommand to separate file

---
+++ rzeczuchyTasks/ViewModel/Commands/AddToDoCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace rzeczuchyTasks.ViewModel.Commands
+{
+    public class AddToDoCommand : ICommand
+    {
+        public AddToDoCommand(MainViewModel viewModel)
+        {
+            ViewModel = viewModel;
+        }
+
+        public MainViewModel ViewModel { get; set; }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (parameter is string label && !string.IsNullOrWhiteSpace(label))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            ViewModel.AddToDo();
+        }
+    }
+}
--- rzeczuchyTasks/ViewModel/MainViewModel.cs
+++ rzeczuchyTasks/ViewModel/MainViewModel.cs
@@ -8,8 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Input;
 using rzeczuchyTasks.Model;
+using rzeczuchyTasks.ViewModel.Commands;
 
 namespace rzeczuchyTasks.ViewModel
 {
@@ -48,36 +48,6 @@ namespace rzeczuchyTasks.ViewModel
         private void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-
-    public class AddToDoCommand : ICommand
-    {
-        public AddToDoCommand(MainViewModel viewModel)
-        {
-            ViewModel = viewModel;
-        }
-
-        public MainViewModel ViewModel { get; set; }
-
-        public event EventHandler CanExecuteChanged
-        {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
-        }
-
-        public bool CanExecute(object parameter)
-        {
-            if (parameter is string label && !string.IsNullOrWhiteSpace(label))
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public void Execute(object parameter)
-        {
-            ViewModel.AddToDo();
         }
     }
 }
