10c960b4f4dfe7f7d49370a736670e0422d79b5c
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-11-16 17:24:01 +0100

    adding unit tests project

---
+++ rzeczuchyTask.UnitTests/DataReaderWriterTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using rzeczuchyTasks.Model;
+
+namespace rzeczuchyTask.UnitTests
+{
+    [TestClass]
+    public class DataReaderWriterTests
+    {
+        [TestMethod]
+        public void NewToDoId_ReturnsProperId()
+        {
+            var testList = new List<ToDo>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                testList.Add(new ToDo(i, "test", false, null));
+
+                Assert.AreEqual(DataReaderWriter.NewToDoId(testList), testList.Count);
+            }
+        }
+    }
+}
---
+++ rzeczuchyTask.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("rzeczuchyTask.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("rzeczuchyTask.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d30564e-0b8a-48e8-a0d8-da4b789b1635")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
